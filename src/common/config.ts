9010d53a578f23e938cc4cd4be20248c9cdd0393
祝传鹏 <Luomusha@gmail.com>
2023-12-17 00:12:04 +0800

    m

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,7 +1,7 @@
 export const DB_HOST = process.env.DB_HOST || "localhost"
-export const DB_PORT = process.env.DB_HOST || 5432
-export const DB_USER = process.env.DB_HOST || "postgres"
-export const DB_PASS = process.env.DB_HOST || "postgres"
+export const DB_PORT = process.env.DB_PORT || 5432
+export const DB_USER = process.env.DB_USER || "postgres"
+export const DB_PASS = process.env.DB_PASS || "postgres"
 
 export const PRIVATE_KEY = process.env.PRIVATE_KEY || "private.pem"
 export const PUBLIC_KEY = process.env.PUBLIC_KEY || "public.pem"
